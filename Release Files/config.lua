53b5686983a4ce23cb23b48c4d3043fe079cbc80
Adrinus <mckibbon@gmail.com>
2019-11-25 13:08:06 -0500

    Update config.lua

--- Release Files/config.lua
+++ Release Files/config.lua
@@ -1,8 +1,8 @@
 --Determines how much the head bobbles as you talk (Higher is more, recommended 0.0-0.1 at max)
 bobbleConstant = 0.02
 -- Open the openmouth.png in an image editor and find the X and Y pixes when your mouse is on the center of the mouth.
-mouthXOff = 161
-mouthYOff = 409
+mouthXOff = 158
+mouthYOff = 395
 -- Set defaults for Audio Setup
 noisegate = 0
 preAmp = 200
